73aa291034a6f8ee1bc2f9cd208aa8b5c6bcc3d5
Tami Mitchell <mail@tameramitchell.com>
2019-05-16 11:35:34 -0700

    Add a simple Mapbox & Mapbox Draw view

--- src/views/Maps/RouteNetworkMap.jsx
+++ src/views/Maps/RouteNetworkMap.jsx
@@ -1,36 +1,46 @@
 import React from "react";
-// react components used to create a google map
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
+import mapboxgl from 'mapbox-gl'
+import MapboxDraw from 'mapbox-gl-draw';
 
-const CustomMap = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        zoomControl: true
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
+import 'mapbox-gl/dist/mapbox-gl.css'
+import 'mapbox-gl-draw/dist/mapbox-gl-draw.css'
 
-function RouteNetworkMap({ ...prop }) {
-  return (
-    <CustomMap
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `100vh` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
-  );
+class RouteNetworkMap extends React.Component {
+  state = {
+    viewport: {
+      latitude: "37.9135",
+      longitude: "-122.2914",
+      zoom: 16,
+      styleID: "mapbox/light-v10"
+    }
+  }
+  componentDidMount(){
+    const { longitude, latitude, zoom, styleID } = this.state.viewport;
+    const mapConfig = {
+      container: 'route-network-map',
+      style: `mapbox://styles/${ styleID }`,
+      center: [longitude, latitude],
+      zoom: zoom,
+    };
+    mapboxgl.accessToken = process.env.REACT_APP_MapboxAccessToken;
+
+    this.map = new mapboxgl.Map(mapConfig);
+
+    const draw = new MapboxDraw();
+    this.map.addControl(draw);
+
+
+    this.map.on('load', () => {
+      const style = this.map.getStyle();
+    });
+  }
+
+  render() {
+    return (
+      <div id="route-network-map" style={{width: '100%', height: '100%'}}>
+      </div>
+    )
+  }
 }
 
 export default RouteNetworkMap;

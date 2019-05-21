8bf11b23a489eee2d420de51a403e00b4349ce77
Tami Mitchell <mail@tameramitchell.com>
2019-05-21 14:32:09 -0700

    Skip auth for now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux"
 import configureStore from './redux/store';
-import netlifyIdentity from 'netlify-identity-widget'
 
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
@@ -14,7 +13,6 @@ import "assets/css/pe-icon-7-stroke.css";
 import AdminLayout from "layouts/Admin.jsx";
 
 const store = configureStore();
-netlifyIdentity.init()
 
 ReactDOM.render(
   <Provider store={store}>

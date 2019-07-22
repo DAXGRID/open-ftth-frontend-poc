e4f34f98180132504ccfd4c99197c1465c727cf6
Tami <mail@tameramitchell.com>
2019-07-21 22:40:59 -0700

    Update icon sizes to not be so tiny

--- src/lib/mapbox/layers/nodes.js
+++ src/lib/mapbox/layers/nodes.js
@@ -1,10 +1,13 @@
-export const nodesLayer = (features) => {
-  return ({
+export const nodesLayer = features => {
+  return {
     id: "featureNodes",
     type: "symbol",
     layout: {
       "icon-image": "{icon}",
-      "icon-allow-overlap": true
+      "icon-allow-overlap": true,
+      "icon-size": {
+        stops: [[10, 0.3], [13, 0.5], [17, 1.25], [20, 3]]
+      }
     },
     source: {
       type: "geojson",
@@ -13,5 +16,5 @@ export const nodesLayer = (features) => {
         features: features
       }
     }
-  })
+  };
 };

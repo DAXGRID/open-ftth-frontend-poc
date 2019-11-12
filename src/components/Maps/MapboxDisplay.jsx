81b0f575a4dcb414ebd2d7f093b91c2254552537
Tami <mail@tameramitchell.com>
2019-11-12 15:04:02 -0800

    Fix draw tools showing on regular map

--- src/components/Maps/MapboxDisplay.jsx
+++ src/components/Maps/MapboxDisplay.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-import EditableFeatures from "./EditableFeatures"
-import UneditableFeatures from "./UneditableFeatures"
+import EditableFeatures from "./EditableFeatures";
+import UneditableFeatures from "./UneditableFeatures";
+import _ from "lodash";
 
 const MapboxDisplay = ({ config, uneditableFeatures, editableFeatures }) => {
   const [map, setMap] = React.useState();
-  const drawEnabled = !!editableFeatures;
 
   React.useLayoutEffect(() => {
     mapboxgl.accessToken = process.env.REACT_APP_MapboxAccessToken;
@@ -17,8 +17,13 @@ const MapboxDisplay = ({ config, uneditableFeatures, editableFeatures }) => {
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <div id={config.container} style={{ width: "100%", height: "100%" }} />
-      <UneditableFeatures map={map} features={uneditableFeatures} />
-      <EditableFeatures map={map} features={editableFeatures} />
+      {uneditableFeatures && _.size(uneditableFeatures) > 0 && (
+        <UneditableFeatures map={map} features={uneditableFeatures} />
+      )}
+
+      {editableFeatures && _.size(editableFeatures) > 0 && (
+        <EditableFeatures map={map} features={editableFeatures} />
+      )}
     </div>
   );
 };

49174212e21a64646aaea8c31fea8321a2fe1ac4
Tami <mail@tameramitchell.com>
2019-12-10 14:08:22 -0800

    Fix Apollo warning

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,18 +1,19 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "https://test02-backend.openftth.net/graphql",
-  defaultOptions: {
-    watchQuery: {
-      errorPolicy: 'all'
-    },
-    query: {
-      errorPolicy: 'all'
-    },
-    mutate: {
-      errorPolicy: 'all'
-    }
-  },
+  uri: "https://test02-backend.openftth.net/graphql"
 });
+
+client.defaultOptions = {
+  watchQuery: {
+    errorPolicy: "all"
+  },
+  query: {
+    errorPolicy: "all"
+  },
+  mutate: {
+    errorPolicy: "all"
+  }
+};
 
 export default client;
